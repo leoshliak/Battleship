5dc403e51a25cf1003c0d926f463444f9ef44c3f
leoshliak <levshlyak@gmail.com>
2024-08-23 11:53:43 +0300

    placeShip test

--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -2,5 +2,14 @@ const Gameboard = require('../scripts/gameboard');
 
 
 describe('Gameboard class', () => {
-    
+    let  gameboard = new Gameboard();
+
+    test('places a ship of valid length and direction', () => {
+        gameboard.placeShip(2, 1, 0, 'vertical');
+        const placedShip = gameboard.board[0];
+        expect(placedShip.coordinates).toEqual([
+          { x: 1, y: 0 },
+          { x: 1, y: 1 }
+        ]);
+      });
 })
